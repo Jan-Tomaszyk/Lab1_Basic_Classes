18ba4cad53bdfaf76a1996850a1776f44bca17b8
Jan-Tomaszyk <jantomaszyk@gmail.com>
2025-10-15 22:07:44 +0200

    car2-3

--- main.cpp
+++ main.cpp
@@ -11,8 +11,10 @@ class Car {
         string make;
         string model;
         int year;
+        int currentYear=2025;
     public:
     // Constructor
+        //int year;
         Car (string mak, string mod, int y)
         {
             make=mak;
@@ -23,16 +25,37 @@ class Car {
     // Method to display car information
         void displayInfo()
         {
-            cout<<make<<" "<<model<<" "<<year<<"\n";
+            cout<<"\n"<<make<<" "<<model<<" "<<year<<"\n";
+        }
+
+        int getCarAge()
+        {
+            return currentYear-year;
+        }
+        bool isSameCar(Car otherCar)
+        {
+            return (this->make==otherCar.make) && (this->model==otherCar.model) && (this->year==otherCar.year);
         }
 };
-
+//Czêœæ 3 odpowiedzi
+//1. Dlatego, ¿e dziêki temu zapobiegamy ich niechcianej modyfikacji przez kod spoza metod klasy.
+//2. Wtedy bêdzie mo¿na modyfikowaæ ich wartoœæ komendami spoza metod klasy.
+//3. Tak, tak.
 int main() {
     // Create a Car object
     Car my("Opel", "Opel 3", 2002);
+    Car concepted("Opel", "Opel 3", 2005);
+    Car born("Opel", "Opel 3", 2005);
+    Car sister("Mazda", "Santa Fe", 2020);
     // Display car details
+    //my.year=2005;
     my.displayInfo();
+    cout<<" "<<my.getCarAge()<<"\n";
+    born.displayInfo();
+    cout<<" "<<born.getCarAge()<<"\n";
+    sister.displayInfo();
+    cout<<" "<<sister.getCarAge()<<"\n\n";
 
-
+    cout<<my.isSameCar(born)<<" "<<born.isSameCar(concepted);
     return 0;
 }
