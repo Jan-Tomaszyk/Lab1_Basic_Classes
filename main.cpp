95b22a68e8f76919960a770deb255604da78c27a
Jan Tomaszyk <jantomaszyk@gmail.com>
2025-10-15 14:20:20 +0200

    car1

--- main.cpp
+++ main.cpp
@@ -1,18 +1,38 @@
-// include the necessary libraries
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
 
 class Car {
+    private:
     //attributes
-
+        string make;
+        string model;
+        int year;
+    public:
     // Constructor
-
+        Car (string mak, string mod, int y)
+        {
+            make=mak;
+            model=mod;
+            year=y;
+        }
+        ~Car(){}
     // Method to display car information
-   
+        void displayInfo()
+        {
+            cout<<make<<" "<<model<<" "<<year<<"\n";
+        }
 };
 
 int main() {
     // Create a Car object
-    
+    Car my("Opel", "Opel 3", 2002);
     // Display car details
-    
+    my.displayInfo();
+
+
     return 0;
 }
